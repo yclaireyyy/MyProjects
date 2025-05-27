1cc3472d3a968fab583424d1fc37f9f94dbc2b60
yclaireyyy <yaqiyang1@student.unimelb.edu.au>
2025-05-27 21:14:33 +1000

    2

--- agents/t_052/myTeam.py
+++ agents/t_052/myTeam.py
@@ -12,6 +12,19 @@ from collections import defaultdict
 MAX_THINK_TIME = 0.95
 HOTB_COORDS = [(4, 4), (4, 5), (5, 4), (5, 5)]
 
+BOARD = [['jk','2s','3s','4s','5s','6s','7s','8s','9s','jk'],
+         ['6c','5c','4c','3c','2c','ah','kh','qh','th','ts'],
+         ['7c','as','2d','3d','4d','5d','6d','7d','9h','qs'],
+         ['8c','ks','6c','5c','4c','3c','2c','8d','8h','ks'],
+         ['9c','qs','7c','6h','5h','4h','ah','9d','7h','as'],
+         ['tc','ts','8c','7h','2h','3h','kh','td','6h','2d'],
+         ['qc','9s','9c','8h','9h','th','qh','qd','5h','3d'],
+         ['kc','8s','tc','qc','kc','ac','ad','kd','4h','4d'],
+         ['ac','7s','6s','5s','4s','3s','2s','2h','3h','5d'],
+         ['jk','ad','kd','qd','td','9d','8d','7d','6d','jk']]
+
+COORDS = {}
+
 
 TTEntry = namedtuple('TTEntry', 'depth score flag best_move')
 # flag: 'EXACT', 'LOWER', 'UPPER'
